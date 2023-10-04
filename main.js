9ddd84d1abe31ba75796a41c0a160aadc978fad1
Nerm <nerm.animator@gmail.com>
2023-10-04 11:58:53 -0700

    Creating my first namespace

--- main.js
+++ main.js
@@ -1,61 +1,12 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: [1, 2]
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: [1],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor() {
-            this.age = 0;
-            this.name = '';
-            this.realName = '';
-        }
-        mutantPower(id) {
-            return this.name + ' ' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Fercho',
-        age: 25,
-        address: {
-            id: 123,
-            zip: 'Ky2 Sud',
-            city: 'Ontario'
-        },
-        getFullAddress() {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Meli',
-        age: 22,
-        address: {
-            id: 126,
-            zip: 'Ytz Ame',
-            city: 'Tejax'
-        },
-        getFullAddress() {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+    console.log(Validations.validateText('Nerm'));
+})(Validations || (Validations = {}));
 //# sourceMappingURL=main.js.map
